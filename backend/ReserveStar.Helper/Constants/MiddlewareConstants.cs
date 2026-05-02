9931ef8278a98bfd9fc27bb9398a869d1ae38d07
computeneer <computeneer@gmail.com>
2026-05-02 19:55:16 +0300

    fix cache name

--- backend/ReserveStar.Helper/Constants/MiddlewareConstants.cs
+++ backend/ReserveStar.Helper/Constants/MiddlewareConstants.cs
@@ -2,6 +2,6 @@ namespace ReserveStar.Helper.Constants;
 
 public static class MiddlewareConstants
 {
-   public const string SuperAdminBypassAuthenticationMiddlewareKey = "stockama_superadmin_bypass_authentication_middleware";
+   public const string SuperAdminBypassAuthenticationMiddlewareKey = "reservestar_superadmin_bypass_authentication_middleware";
    public const string RefreshedAccessTokenHeaderName = "X-Access-Token";
 }
