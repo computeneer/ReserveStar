8bf4ab91e5b6a43669163473fca8851e52042bee
computeneer <computeneer@gmail.com>
2026-05-02 22:07:45 +0300

    add companycode to baserequest

--- backend/ReserveStar.Helper/Api/BaseRequest.cs
+++ backend/ReserveStar.Helper/Api/BaseRequest.cs
@@ -7,4 +7,5 @@ public class BaseRequest<T> : IRequest<T>
    public Guid UserId { get; set; }
    public Guid LanguageId { get; set; }
    public Guid CompanyId { get; set; }
+   public string CompanyCode { get; set; }
 }
